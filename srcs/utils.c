ca63af67f49a024af6eaf43b88e82f88180c307b
moi <alexdiehlboitel@gmail.com>
2025-03-04 21:26:22 +0100

    forgor

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,5 +1,30 @@
 #include "philosophers.h"
 
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	result;
+	int	sign;
+
+	sign = 1;
+	i = 0;
+	result = 0;
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if ((nptr[i] == '-' || nptr[i] == '+') && nptr[i])
+	{
+		if (nptr[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (nptr[i] && (ft_isdigit(nptr[i]) == 1))
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
+
 int	ft_write(char *str)
 {
 	write(2, str, ft_strlen(str));
