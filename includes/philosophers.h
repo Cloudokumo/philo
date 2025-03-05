eca4bf6d9b524610629829eecc7205d46dff936c
moi <alexdiehlboitel@gmail.com>
2025-03-05 19:14:07 +0100

    init

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -7,6 +7,18 @@
 #include <unistd.h>
 #include <sys/time.h>
 
+typedef struct s_philo
+{
+	int				nbr_eat;
+    long	        last_meal;
+	int				pos;
+	char			*pos_str;
+	int				lfork;
+	int				rfork;
+	struct s_data	*data;
+	pthread_t		thread_id;
+}				t_philo;
+
 typedef struct s_data
 {
     int     count;
@@ -14,6 +26,10 @@ typedef struct s_data
     int     t_eat;
     int     t_sleep;
     int     eat_max;
+    t_philo			*philos;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	meal;
+	pthread_mutex_t	print;
 }           t_data;
 
 #endif
---
+++ initialization.c
@@ -0,0 +1,74 @@
+#include "philosophers.h"
+
+int	ft_mutex(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->count)
+	{
+		if (pthread_mutex_init(&(data->forks[i]), NULL))
+			return (1);
+		i++;
+	}
+	if (pthread_mutex_init(&data->meal, NULL))
+		return (1);
+	if (pthread_mutex_init(&data->print, NULL))
+		return (1);
+	return (0);
+}
+
+int	ft_philo(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->count)
+	{
+		data->philos[i].nbr_eat = 0;
+		data->philos[i].pos = i + 1;
+		data->philos[i].pos_str = ft_itoa(i + 1);
+		if (!data->philos[i].pos_str)
+			break ;
+		data->philos[i].rfork = i;
+		data->philos[i].lfork = (i + 1) % data->count;
+		data->philos[i].data = data;
+		i++;
+	}
+	if (i != data->count)
+	{
+		while (i >= 0)
+		{
+			free(data->philos[i].pos_str);
+			i--;
+		}
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_init(t_data *data)
+{
+	data->philos = malloc(sizeof(t_philo) * data->count);
+	if (!data->philos)
+		return (0);
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->count);
+	if (!data->forks)
+	{
+		free(data->philos);
+		return (0);
+	}
+	if (ft_mutex(data))
+	{
+		free(data->philos);
+		free(data->forks);
+		return (0);
+	}
+	if (ft_philo(data))
+	{
+		free(data->philos);
+		free(data->forks);
+		return (0);
+	}
+	return (1);
+}
